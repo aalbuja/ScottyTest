d09cec215a52661b98ae3840e799c739b079f70c
Andrea Albuja <aalbuja@stackbuilders.com>
2015-08-19 12:11:55 -0500

    Add import System.Environment and Control.Monad

--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Web.Scotty
+import System.Environment
+import Control.Monad
 
 main :: IO ()
-main = scotty 3000 $ do
-  get "/person" $ do
-    text "gotten!"
+main = do
+  port <- liftM read $ getEnv "PORT"
+  scotty  port  $ do
+    
+   get "/" $ do
+     text "gotten!"
   
-  delete "/" $ do
-    text "deleted!"
+   delete "/" $ do
+     text "deleted!"
 
-  post "/" $ do
-    text "postded!"
+   post "/" $ do
+     text "postded!"
 
-  put "/" $ do
-    text "put-ted!"
+   put "/" $ do
+     text "put-ted!"
